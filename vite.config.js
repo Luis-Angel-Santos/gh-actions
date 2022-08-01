35159b1c292d6aab5d4911e027165a87fd84eb58
Luis Angel Santos <santosangel748@gmail.com>
2022-08-01 18:57:51 -0500

    arreglando bug 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://Luis-Angel-Santos.github.io/',
+  base: 'https://luis-angel-santos.github.io/',
   plugins: [react()]
 })
